78f93f4f48838fa583bb69fd769b9f87767f472e
Alexis Cespedes <l22260112@matamoros.tecnm.mx>
2026-04-19 11:43:44 -0500

    feat:validate login data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,16 +15,19 @@ class AuthController extends Controller
     }
 
     public function login(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => 'required | string | email',
-            'password' => 'required | string',
-        ]);
+{
+    $credentials = $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+    ]);
 
-        if (Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Login successful'], 200);
-        } else {
-            return response()->json(['message' => 'Invalid credentials'], 401);
-        }
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->intended('/dashboard'); 
     }
+
+    return back()->withErrors([
+        'email' => 'El correo o la contraseña son incorrectos.',
+    ])->onlyInput('email');
+}
 }
